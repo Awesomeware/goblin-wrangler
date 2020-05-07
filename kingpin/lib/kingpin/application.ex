09358e08c3e65c50d27ec049a3cf72a60559df79
Stephen Badger <stephen.badger@gmail.com>
2020-05-07 20:06:55 +0100

    Adds migration function

--- kingpin/lib/kingpin/application.ex
+++ kingpin/lib/kingpin/application.ex
@@ -32,4 +32,35 @@ defmodule Kingpin.Application do
     KingpinWeb.Endpoint.config_change(changed, removed)
     :ok
   end
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      :ok = ensure_repo_created(repo)
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp ensure_repo_created(repo) do
+    IO.puts "create #{inspect repo} database if it doesn't exist"
+    case repo.__adapter__.storage_up(repo.config) do
+      :ok -> :ok
+      {:error, :already_up} -> :ok
+      {:error, term} -> {:error, term}
+    end
+  end
+
+  defp repos do
+    Application.fetch_env!(:kingpin, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(:kingpin)
+  end
 end
