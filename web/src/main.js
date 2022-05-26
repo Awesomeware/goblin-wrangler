5d52f3057f2ae1fc8324295cdc015dfda32e759a
Martin Grunbaum <martin@itsolveonline.net>
2022-05-26 13:23:33 +0200

    Add Vuetify

--- web/src/main.js
+++ web/src/main.js
@@ -4,5 +4,14 @@ import { createPinia } from "pinia";
 import App from "@/App.vue";
 import router from "@/router";
 import auth from "@/plugins/auth";
+import vuetify from "@/plugins/vuetify";
+import { loadFonts } from "@/plugins/webfontloader";
 
-createApp(App).use(createPinia()).use(auth).use(router).mount("#app");
+loadFonts();
+
+createApp(App)
+  .use(createPinia())
+  .use(auth)
+  .use(router)
+  .use(vuetify)
+  .mount("#app");
